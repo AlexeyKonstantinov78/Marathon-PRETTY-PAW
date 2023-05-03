270df37838a88444d248bb9c1f8a8c22f23b05aa
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-05-03 10:33:38 +0300

    add feat webp convertor

---
+++ webp-convert.mjs
@@ -0,0 +1,12 @@
+import imagemin from "imagemin";
+import imageminWebp from "imagemin-webp";
+
+await imagemin(['src/img/*.{jpg,png}'], {
+  destination: 'src/img',
+  plugins: [
+    imageminWebp({quality: 85}),
+
+  ]
+});
+
+console.log(`Конвертация в webp завершенна`);
