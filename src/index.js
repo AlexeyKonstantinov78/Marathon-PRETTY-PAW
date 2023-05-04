0b3ff8e20404e46a68c9c6a66da7044733823b7a
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-05-04 13:05:29 +0300

    add style hero and video

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import './index.html';
 
 
 import './index.scss';
+
+//use modules
+
+const videoBG = document.querySelector('.video-bg');
+
+videoBG.innerHTML = `
+  <source src="video/video.webm" type="video/webm">
+  <source src="video/video.mp4" type="video/mp4">
+`;
