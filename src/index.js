c98fd5ba8c258c32afb50f4555873f4b154b9d79
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-05-05 23:15:17 +0300

    feat add style career and activation swiper

--- src/index.js
+++ src/index.js
@@ -14,6 +14,33 @@ slidersInit('.about__slider', {
   }
 });
 
+slidersInit('.career__slider', {
+  pagination: {
+    el: '.career__slider-pagination'
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 26
+    },
+    1240: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
+  }
+});
+
 const videoBG = document.querySelector('.video-bg');
 
 videoBG.innerHTML = `
